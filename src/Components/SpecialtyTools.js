f20259c5843afc76d15eae136e6441ed5af69394
AdamRayPublic <aray58@gmail.com>
2022-01-31 09:05:30 -0600

    css edits for responsive

--- src/Components/SpecialtyTools.js
+++ src/Components/SpecialtyTools.js
@@ -37,7 +37,7 @@ export default function SpecialtyTools() {
                 <a target= "_blank" href="https://firebasestorage.googleapis.com/v0/b/deep-south-chemical.appspot.com/o/Technical%20Briefs%2FBenefits%20of%20BOP%20Stack%20Cleaning.pdf?alt=media&token=f269987e-021f-4e4a-b2b3-a5e0f45091a5">Benefits of BOP Stack Cleaning</a>
                 <a target= "_blank" href="https://firebasestorage.googleapis.com/v0/b/deep-south-chemical.appspot.com/o/Technical%20Briefs%2FPrinciples%20of%20Mechanical%20Cleaning%20Tools.pdf?alt=media&token=5825190e-8c55-40c6-8527-b9e9cde3ab9a">Principles of Mechanical Cleaning Tools</a>
                 <a target= "_blank" href="https://firebasestorage.googleapis.com/v0/b/deep-south-chemical.appspot.com/o/Technical%20Briefs%2FUtilization%20of%20Mechanical%20Cleaning%20Tools.pdf?alt=media&token=ebc82193-d2b3-46e3-93fd-8499ff30e7a8">Utilization of Mechanical Cleaning Tools</a>
-            <h4>Products</h4>    
+            <h2>Products</h2>    
         <div>
             {error && <p className="error">{error}</p>}
             {documents && (
